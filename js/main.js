a63284283ea0af6ef7f342b173f146bbd7893a13
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-05-19 14:24:44 +1000

    island deployed

--- js/main.js
+++ js/main.js
@@ -3,11 +3,45 @@ $( document ).ready(function() {
   var p = $("#potion").click(function() {
     console.log("I work");
      $('.potion').addClass('shrink');
-
      $( ".bubbles" ).fadeOut( 2000);
-//      $( ".bubbles" ).fadeOut( "1500", function() {
-//   // Animation complete.
-// });
-
+     $( ".castle" ).addClass('hide');
   });
+
 });
+
+// var leftIndex = 0;
+// var rightIndex = 0;
+//
+// $( document ).mousemove(  function( event ) {
+//
+//
+//   mousePos = {
+//              x: event.pageX,
+//              y: event.pageY
+//          };
+//
+//   var x = $( window ).width() / 2;
+//   var y = $( window ).height() / 2;
+//
+//
+// if(mousePos.x >= x ){
+//   $('.castle').css("left", leftIndex+'px' )
+// leftIndex --;
+// };
+//
+// if(mousePos.x <= x ){
+//   $('.castle').css("left", leftIndex+'px' )
+//   leftIndex ++;
+// };
+//
+// if(mousePos.y >= y ){
+//   $('.castle').css("top", leftIndex+'px' )
+// leftIndex --;
+// };
+//
+// if(mousePos.y <= y ){
+//   $('.castle').css("top", leftIndex+'px' )
+//   leftIndex ++;
+// };
+// 
+// })
