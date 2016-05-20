80601a89752d4f6fe60f497f57f4d475de88eb60
Tom Ulman <Tom@Toms-Macbook-Pro.local>
2016-05-20 13:31:28 +1000

    working on styling, text and links, jack has helped to create the mouse parralax

--- js/main.js
+++ js/main.js
@@ -1,48 +1,62 @@
 $(document).ready(function() {
 
   var p = $("#potion").click(function() {
-    console.log("I work");
     $('.potion').addClass('shrink');
     $(".bubbles").fadeOut(2000);
     $(".castle").addClass('hide');
 
-    $(".test").addClass('hideme');
+    $(".whitesidediv").addClass('hideme');
 
-
+    // Reveals
     $(".hidden").css("display", "block")
 
-    var p = $("body").mousemove(function() {
 
-      $(".castle").addClass('float');
-    });
+    // Adds delay so castle can slowly appear before floating starts
+    $(".castle").delay(1000).queue(function(next){
+    $(this).addClass("float");
+    next();
+});
+  });
   });
 
+//  Jack movement
+// chuck this in the css for a smooth ride
+//
+//    -webkit-transition: left 0.1s linear, top 0.1s linear;
+    // -moz-transition: left 0.1s linear, top 0.1s linear;
+    // -ms-transition: left 0.1s linear, top 0.1s linear;
+    // -o-transition: left 0.1s linear, top 0.1s linear;
+    // transition: left 0.1s linear, top 0.1s linear;
 
-
-// "https://www.youtube.com/watch?v=DVg2EJvvlF8"
-  $(".dream").click(function(){
-    var link = $(this);
-      var target = link.attr("target");
-
-      if($.trim(target).length > 0)
-      {
-        window.open(link.attr("href"), target);
-      }
-      else
-      {
-         window.location = link.attr("href");
-      }
-
-      event.preventDefault();  });
-});
-
-
-
-
-
+// $(document).mousemove(function (e) {
+//   var xCoord = event.pageX;
+//   var yCoord = event.pageY;
+//
+//   var halfWidth = $(window).width() / 4;
+//   var halfHeight = $(window).height() / 4;
+//
+//   var differenceX = xCoord - halfWidth;
+//   var differenceY = yCoord - halfHeight;
+//
+//   var scaledDifferenceX = differenceX / 6;
+//   var scaledDifferenceY = differenceY / 6;
+//
+//   $(".castle").css({
+//     left: scaledDifferenceX + "px",
+//     top: scaledDifferenceY + "px"
+//   });
+//
+//   console.log( differenceX, differenceY, scaledDifferenceX, scaledDifferenceY );
+// });
+//
 // var leftIndex = 0;
 // var rightIndex = 0;
-//
+
+
+/////////////////////// Kane Below
+
+
+
 // $( document ).mousemove(  function( event ) {
 //
 //
@@ -65,14 +79,14 @@ $(document).ready(function() {
 //   leftIndex ++;
 // };
 //
-// if(mousePos.y >= y ){
-//   $('.castle').css("top", leftIndex+'px' )
-// leftIndex --;
-// };
-//
-// if(mousePos.y <= y ){
-//   $('.castle').css("top", leftIndex+'px' )
-//   leftIndex ++;
-// };
+// // if(mousePos.y >= y ){
+// //   $('.castle').css("top", leftIndex+'px' )
+// // leftIndex --;
+// // };
+// //
+// // if(mousePos.y <= y ){
+// //   $('.castle').css("top", leftIndex+'px' )
+// //   leftIndex ++;
+// // };
 //
 // })
